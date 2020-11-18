b56c013491cb7a3d892f21066166f90c80ba68f8
Berke Esmer <berke.esmer@tum.de>
2020-11-18 09:19:45 +0100

    BaseActivity is created

---
+++ DeviceTracking/app/src/main/java/com/alpsproject/devicetracking/BaseActivity.kt
@@ -0,0 +1,19 @@
+package com.alpsproject.devicetracking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+This activity is to be inherited by all living activities in the application
+BaseActivity includes functions that is most common and shared by all activities.
+ */
+class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun setTitle(title: CharSequence?) {
+        super.setTitle(title)
+    }
+}
