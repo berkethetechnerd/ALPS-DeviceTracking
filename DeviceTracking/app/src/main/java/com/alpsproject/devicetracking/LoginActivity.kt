b405fb22cad1ed701f133cb80ae308cc9ede146b
Ece Ata <eceatata@hotmail.com>
2020-12-03 13:50:11 +0100

    Login activity added

---
+++ DeviceTracking/app/src/main/java/com/alpsproject/devicetracking/LoginActivity.kt
@@ -0,0 +1,31 @@
+package com.alpsproject.devicetracking
+
+import android.media.tv.TvContract
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+
+class LoginActivity : BaseActivity() {
+
+    private lateinit var ivLogo: ImageView
+    private lateinit var tvTitle: TextView
+    private lateinit var tvDisclamer: TextView
+    private lateinit var btnNext: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        ivLogo = findViewById(R.id.iv_logo)
+        tvTitle = findViewById(R.id.tv_title)
+        tvDisclamer = findViewById(R.id.tv_disclaimer_text)
+        btnNext = findViewById(R.id.btn_next)
+
+        tvTitle.text = getString(R.string.login_title)
+        tvDisclamer.text = getString(R.string.login_disclaimer)
+
+        btnNext.setOnClickListener {
+            Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
