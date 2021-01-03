0979bda79a1c862bba619659c94534e2e61dbc34
Ece Ata <eceatata@hotmail.com>
2021-01-03 23:45:59 +0100

    added permission manager for mobile data

--- DeviceTracking/app/src/main/java/com/alpsproject/devicetracking/helper/PermissionManager.kt
+++ DeviceTracking/app/src/main/java/com/alpsproject/devicetracking/helper/PermissionManager.kt
@@ -10,6 +10,7 @@ object PermissionManager {
             AccessSensor.ACCESS_WIFI -> SharedPreferencesManager.read(ConstantsManager.CONSENT_OF_WIFI, false)
             AccessSensor.ACCESS_BLUETOOTH -> SharedPreferencesManager.read(ConstantsManager.CONSENT_OF_BLUETOOTH, false)
             AccessSensor.ACCESS_SCREEN_USAGE -> SharedPreferencesManager.read(ConstantsManager.CONSENT_OF_SCREEN_USAGE, false)
+            AccessSensor.ACCESS_MOBILE_DATA -> SharedPreferencesManager.read(ConstantsManager.CONSENT_OF_MOBILE_DATA, false)
         }
     }
 
@@ -18,6 +19,7 @@ object PermissionManager {
             AccessSensor.ACCESS_WIFI -> UserMessageGenerator.generateDialogForPermission(context, AccessSensor.ACCESS_WIFI)
             AccessSensor.ACCESS_BLUETOOTH -> UserMessageGenerator.generateDialogForPermission(context, AccessSensor.ACCESS_BLUETOOTH)
             AccessSensor.ACCESS_SCREEN_USAGE -> UserMessageGenerator.generateDialogForPermission(context, AccessSensor.ACCESS_SCREEN_USAGE)
+            AccessSensor.ACCESS_MOBILE_DATA -> UserMessageGenerator.generateDialogForPermission(context, AccessSensor.ACCESS_MOBILE_DATA)
         }
     }
 
