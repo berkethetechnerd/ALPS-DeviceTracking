99e2670d529614eb7501e387da08c3f859ad437d
Ece Ata <eceatata@hotmail.com>
2021-01-03 23:45:43 +0100

    added settings manager for mobile data

--- DeviceTracking/app/src/main/java/com/alpsproject/devicetracking/helper/SettingsManager.kt
+++ DeviceTracking/app/src/main/java/com/alpsproject/devicetracking/helper/SettingsManager.kt
@@ -4,12 +4,16 @@ import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.PowerManager
 import android.provider.Settings
+import androidx.core.content.ContextCompat.getSystemService
 import com.alpsproject.devicetracking.delegates.ActivationDelegate
 import com.alpsproject.devicetracking.enums.AccessSensor
+
 
 object SettingsManager: ActivationDelegate {
 
@@ -25,7 +29,16 @@ object SettingsManager: ActivationDelegate {
 
     fun askForBluetooth(activity: Activity) {
         if (PermissionManager.checkPermission(AccessSensor.ACCESS_BLUETOOTH) && !isBluetoothEnabled()) {
-            UserMessageGenerator.generateDialogForActivation(activity, AccessSensor.ACCESS_BLUETOOTH)
+            UserMessageGenerator.generateDialogForActivation(
+                activity,
+                AccessSensor.ACCESS_BLUETOOTH
+            )
+        }
+    }
+
+    fun askForMobileData(activity: Activity) {
+        if (PermissionManager.checkPermission(AccessSensor.ACCESS_MOBILE_DATA) && !isMobileDataEnabled(activity)) {
+            UserMessageGenerator.generateDialogForActivation(activity, AccessSensor.ACCESS_MOBILE_DATA)
         }
     }
 
@@ -41,11 +54,18 @@ object SettingsManager: ActivationDelegate {
         return powerManager.isInteractive
     }
 
+    fun isMobileDataEnabled(activity: Activity): Boolean {
+        val connectivityManager = getConnectivityManager(activity) ?: return false // Adapter not found
+        val activeInfo: NetworkInfo? = connectivityManager.activeNetworkInfo
+        return activeInfo?.type == ConnectivityManager.TYPE_MOBILE
+    }
+
     override fun sensorActivated(context: Activity, sensor: AccessSensor) {
         when (sensor) {
             AccessSensor.ACCESS_WIFI -> activateWifi(context)
             AccessSensor.ACCESS_BLUETOOTH -> activateBluetooth()
             AccessSensor.ACCESS_SCREEN_USAGE -> return
+            AccessSensor.ACCESS_MOBILE_DATA -> return
         }
     }
 
@@ -61,9 +81,17 @@ object SettingsManager: ActivationDelegate {
 
     private fun activateBluetooth() = getBluetoothAdapter().enable()
 
-    private fun getWifiManager(activity: Activity): WifiManager? = activity.applicationContext.getSystemService(Context.WIFI_SERVICE) as? WifiManager?
+    private fun getWifiManager(activity: Activity): WifiManager? = activity.applicationContext.getSystemService(
+        Context.WIFI_SERVICE
+    ) as? WifiManager?
 
     private fun getBluetoothAdapter(): BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 
-    private fun getPowerManager(activity: Activity): PowerManager? = activity.getSystemService(Context.POWER_SERVICE) as? PowerManager?
+    private fun getPowerManager(activity: Activity): PowerManager? = activity.getSystemService(
+        Context.POWER_SERVICE
+    ) as? PowerManager?
+
+    private fun getConnectivityManager(activity: Activity): ConnectivityManager? = activity.applicationContext.getSystemService(
+        Context.CONNECTIVITY_SERVICE
+    ) as? ConnectivityManager?
 }
